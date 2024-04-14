aea087b586443c34ed7a521ab71c25030a0837ec
Himanshu Sharma <111674774+Himanshu4256@users.noreply.github.com>
2024-04-14 18:46:48 +0530

    Delete DynamicFormMs directory

--- DynamicFormMs/src/main/java/com/omnie/forms/repo/FormComponentRepository.java
+++
@@ -1,9 +0,0 @@
-package com.omnie.forms.repo;
-import org.springframework.data.mongodb.repository.MongoRepository;
-import org.springframework.stereotype.Repository;
-
-import com.omnie.forms.model.FormComponent; 
-  
-public interface FormComponentRepository extends MongoRepository<FormComponent,String>{ 
-  
-}
